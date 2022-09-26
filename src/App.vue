0511249984c4e28a56f8852f54b556055128bd1b
taekwanpark <techigh@amuz.co.kr>
2022-09-26 10:21:06 +0900

    router set

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script setup>
-import HelloWorld from '@/components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-	<div class="bg-amber-400">
-      <HelloWorld msg="You did it!" />asf
-    </div>
-    <TheWelcome />
+	<RouterView />
 </template>
 
 <style scoped>
