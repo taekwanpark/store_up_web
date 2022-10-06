150473c386503875d36507d865934aa937357eca
taekwanpark <techigh@amuz.co.kr>
2022-10-06 09:54:48 +0900

    env - vite cofig

--- src/components/StickyProducts.vue
+++ src/components/StickyProducts.vue
@@ -1,25 +1,37 @@
 <template>
   <div
-    class="fixed top-1/2 right-20 w-40 -translate-y-1/2 flex-col gap-2 rounded-lg bg-gray-100 p-3 shadow-lg lg:hidden xl:flex"
+    class="fixed top-1/2 right-20 w-32 -translate-y-1/2 rounded-lg bg-transparent px-3 py-4 lg:hidden xl:block"
   >
-    <div
-      class="flex w-full items-center justify-center rounded-lg bg-white shadow-lg"
-    >
-      <div class="rounded-lg p-2 text-sm font-normal">최근 본 상품</div>
-    </div>
-    <div
-      class="flex w-full items-center justify-center rounded-lg bg-white shadow-lg"
-    >
-      <div class="h-64 rounded-lg p-2 text-sm font-light">
-        최근 본 상품이 없습니다.
+    <div class="flex w-full flex-col gap-1">
+      <div
+        class="flex w-full items-center justify-center rounded-lg border-2 bg-white"
+      >
+        <div class="py-2 text-sm font-normal">최근 본 상품</div>
+      </div>
+      <div
+        class="flex w-full items-center justify-center rounded-lg border-2 bg-white"
+      >
+        <div class="h-64 rounded-lg p-2 text-sm font-light">
+          최근 본 상품이 없습니다.
+        </div>
+      </div>
+      <div
+        class="flex w-full items-center justify-center rounded-lg border-2 bg-white"
+      >
+        <div class="py-2 text-sm font-normal">전체 보기</div>
       </div>
     </div>
     <div
-      class="flex w-full items-center justify-center rounded-lg bg-white shadow-lg"
+      class="mt-4 grid grid-cols-5 items-center justify-center rounded-lg bg-gray-500 py-1"
     >
-      <div class="p-2 text-sm font-normal">전체 보기</div>
+      <div class="col-span-3 pb-px text-right text-sm font-light text-white">
+        Top
+      </div>
+      <ArrowSmallUpIcon class="ml-1 h-4 w-4 text-white" />
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ArrowSmallUpIcon } from "@heroicons/vue/24/outline";
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,29 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dns from "dns";
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-
+dns.setDefaultResultOrder("verbatim");
 // https://vitejs.dev/config/
-export default defineConfig( {
-	plugins: [ vue() ],
-	resolve: {
-		extensions: [ '.js', '.vue' ],
-		alias: {
-			'@': fileURLToPath( new URL( './src', import.meta.url ) ),
-		},
-	},
-} );
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    server: {
+      port: 3000,
+    },
+    plugins: [vue()],
+
+    define: {
+      "process.env": loadEnv(mode, process.cwd(), "STORE"),
+      // 'process.env.development': loadEnv( mode, process.cwd() + 'development', 'TECH' ),
+      // 'process.env.production': loadEnv( mode, process.cwd(), 'TECH' ),
+    },
+    resolve: {
+      extensions: [".js", ".vue"],
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  });
+};
